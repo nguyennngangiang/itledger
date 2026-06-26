d81f2b40a2b64d15c18e44519afc35dedd50f967
VNMinh68 <nhatminh12d@gmail.com>
2026-06-26 13:36:00 +0700

    add toasting alert library, clean up App.tsx comments

--- fe/src/types.ts
+++ fe/src/types.ts
@@ -23,9 +23,11 @@ export type User = {
   employee_code: string
   name: string | null
   team: string | null
+  selected: boolean | false
 }
 
 export type UserCreate = Pick<User, 'employee_code'> & Partial<Omit<User, 'employee_code'>>
+export type UserCreateBatch = UserCreate[]
 
 export type Team = {
   team_id: string
@@ -60,3 +62,21 @@ export type Handover = {
 
 export type HandoverCreate = Pick<Handover, 'handover_id'> &
   Partial<Omit<Handover, 'handover_id'>>
+
+export const teamsOptions = [
+    { team_id: "ESG", team_name: "ESG" },
+    { team_id: "DMD", team_name: "DMD" },
+    { team_id: "PMD", team_name: "PMD" },
+    { team_id: "FMD", team_name: "FMD" },
+    { team_id: "PROJECT", team_name: "PROJECT" },
+    { team_id: "MKT", team_name: "MKT" },
+    { team_id: "HR", team_name: "HR" },
+    { team_id: "ACC", team_name: "ACC" },
+    { team_id: "FIN", team_name: "FIN" },
+    { team_id: "ADMIN", team_name: "ADMIN" },
+    { team_id: "IT", team_name: "IT" },
+    { team_id: "S&P", team_name: "S&P" },
+    { team_id: "QA/QC", team_name: "QA/QC" },
+    { team_id: "KRDESK", team_name: "KRDESK" },
+    { team_id: "OPD", team_name: "OPD" },
+  ]; 
